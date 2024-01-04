5956608dbc5871bc14fc6447c208d6f82d902226
kritisha <kritishamoktan530@gmail.com>
2024-01-04 18:19:58 +0545

    added email verification routes and controller

--- routes/auth.php
+++ routes/auth.php
@@ -1,1 +1,9 @@
 <?php
+
+use App\Http\Controllers\Api\Auth\EmailVerificationController;
+
+Route::get('/verify-email/{id}/{hash}', EmailVerificationController::class)
+                ->middleware(['signed', 'throttle:6,1'])
+                ->name('verification.verify');
+
+
