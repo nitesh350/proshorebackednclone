85e379aaea3402a60ef49079151168c42aa8a0a5
nitesh350 <niteshshrestha4321@gmail.com>
2024-01-05 10:27:46 +0545

    crerated features of forgot password

--- routes/auth.php
+++ routes/auth.php
@@ -1,1 +1,8 @@
 <?php
+use App\Http\Controllers\Api\Auth\ForgotPasswordController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('/forgot-password', ForgotPasswordController::class)
+                ->middleware('guest')
+                ->name('password.email');
