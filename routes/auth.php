ee78884dcdb7daa475c0dda6bacc423d1e811d59
nitesh350 <niteshshrestha4321@gmail.com>
2024-01-08 14:11:34 +0545

    add docblock

--- routes/auth.php
+++ routes/auth.php
@@ -1,9 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\ResetPasswordController;
 use App\Http\Controllers\Api\Auth\EmailVerificationController;
 
 Route::get('/verify-email/{id}/{hash}', EmailVerificationController::class)
                 ->middleware(['signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
-
+Route::post('/reset-password', ResetPasswordController::class)
+                ->middleware('guest')
+                ->name('password.store');
