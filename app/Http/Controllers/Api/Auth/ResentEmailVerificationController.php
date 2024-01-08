9cbc7ae0aadd3acbc001b03f42353eda0c05a9b9
kritisha <kritishamoktan530@gmail.com>
2024-01-08 11:51:08 +0545

    resent email verification feature

--- app/Http/Controllers/Api/Auth/ResentEmailVerificationController.php
+++ app/Http/Controllers/Api/Auth/ResentEmailVerificationController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 class ResentEmailVerificationController extends Controller
 {
     /**
+     * @param Request $request
      * @return JsonResponse
      * @return RedirectResponse
      */
--- routes/auth.php
+++ routes/auth.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\EmailVerificationController;
 use App\Http\Controllers\Api\Auth\ResentEmailVerificationController;
 
@@ -7,6 +8,6 @@ Route::get('/verify-email/{id}/{hash}', EmailVerificationController::class)
                 ->middleware(['signed', 'throttle:6,1'])
                 ->name('verification.verify');
 
- Route::post('/email/verification/resend', ResentEmailVerificationController::class)
+Route::post('/email/verification/resend', ResentEmailVerificationController::class)
                 ->middleware(['auth:sanctum', 'throttle:6,1'])
                 ->name('verification.send');
