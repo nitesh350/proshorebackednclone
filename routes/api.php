c1feb52bea635f71abb3ea970b09fc18bda1bc78
Ranjan Nagarkoti <ranjan.ngr2058@gmail.com>
2024-01-09 10:02:22 +0545

    fix folder structure

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProfileController;
+use App\Http\Controllers\Api\Auth\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
