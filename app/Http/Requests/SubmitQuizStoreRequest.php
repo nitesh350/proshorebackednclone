9ad99dd75c6499ebbb7efabc90030f939463f042
Tushar Luitel <tusharluitel88@gmail.com>
2024-01-24 12:28:02 +0545

    enhancement:added validation to calculate result

--- app/Http/Requests/SubmitQuizStoreRequest.php
+++ app/Http/Requests/SubmitQuizStoreRequest.php
@@ -23,8 +23,8 @@ class SubmitQuizStoreRequest extends FormRequest
     {
         return [
             'answers'=>'required|array',
-            'total_question'=>'required|integer',
-            'total_time'=>'required|integer'
+            'total_question'=>'required|integer|min:1',
+            'total_time'=>'required|integer|min:1'
         ];
     }
 }
