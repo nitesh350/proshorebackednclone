3019becc920bdc7d638512add364414ff0e9a344
kritisha <kritishamoktan530@gmail.com>
2024-01-24 17:15:20 +0545

    added filter for quiz for student

--- app/Http/Controllers/Api/Student/GetQuizzesController.php
+++ app/Http/Controllers/Api/Student/GetQuizzesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Student;
 
+use App\Http\Requests\GetQuizzesFilterRequest;
 use App\Models\Quiz;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -11,13 +12,25 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class GetQuizzesController extends Controller
 {
-     /**
+    /**
      * @param Request $request
      * @return AnonymousResourceCollection
-      */
-    public function __invoke(Request $request): AnonymousResourceCollection
+     */
+    public function __invoke(GetQuizzesFilterRequest $request): AnonymousResourceCollection
     {
-        $quizzes = Quiz::with('result')->paginate(10);
-        return QuizResource::collection($quizzes);
+        $data = $request->validated();
+
+        $query = Quiz::with('category:id,title')
+            ->with('result');
+
+        if (!empty($data['title'])) {
+            $query = $query->where('title', 'like', '%' . $data['title'] . '%');
+        }
+
+        if (!empty($data['category_id'])) {
+            $query->where('category_id', $data['category_id']);
+        }
+
+        return QuizResource::collection($query->paginate(10));
     }
 }
