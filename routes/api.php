801e4085a70c7dcc7b2316428f1393366f97fbb5
nitesh350 <niteshshrestha4321@gmail.com>
2024-01-26 19:43:38 +0545

    admin can view all registered students

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth:sanctum'], function () 
     Route::apiResource('/quizzes', QuizController::class)->middleware('ability:manage-quizzes');
     Route::apiResource('/questions', QuestionController::class)->middleware('ability:manage-questions');
     Route::apiResource('/results', ResultController::class)->only(['index'])->middleware('ability:manage-results');
-    Route::apiResource('students/all', StudentController::class)->only(['index'])->middleware('ability:manage-students');
+    Route::apiResource('/students/all', StudentController::class)->only(['index'])->middleware('ability:manage-students');
 });
 
 Route::group(['prefix' => 'student', 'middleware' => 'auth:sanctum'], function () {
