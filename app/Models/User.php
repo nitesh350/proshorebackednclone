f23174080c0a611600aa17b95be37a7b53cdafdc
nishusapkota <nisa_csit2075@lict.edu.np>
2024-01-29 13:27:18 +0545

    issue in middleware for student route

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -48,5 +50,10 @@ class User extends Authenticatable implements MustVerifyEmail
     public function profile(): HasOne
     {
         return $this->hasOne(Profile::class, 'user_id');
+    }
+
+    public function results(): HasMany
+    {
+        return $this->hasMany(Result::class, 'user_id');
     }
 }
--- routes/auth.php
+++ routes/auth.php
@@ -14,9 +14,24 @@ Route::post('/register', RegisterUserController::class)
     ->middleware('guest')
     ->name('register');
 
-Route::post('/login', LoginUserController::class)
+Route::group(['middleware' => ['verified']], function () {
+
+    Route::post('/login', LoginUserController::class)
+        ->middleware('guest')
+        ->name('login');
+
+    Route::post('/logout', LogoutController::class)
+        ->middleware('auth:sanctum')
+        ->name('logout');
+
+    Route::post('/reset-password', ResetPasswordController::class)
+        ->middleware('guest')
+        ->name('password.store');
+});
+
+Route::post('/forgot-password', ForgotPasswordController::class)
     ->middleware('guest')
-    ->name('login');
+    ->name('password.email');
 
 Route::get('/verify-email/{id}/{hash}', EmailVerificationController::class)
     ->middleware(['signed', 'throttle:6,1'])
@@ -25,15 +40,3 @@ Route::get('/verify-email/{id}/{hash}', EmailVerificationController::class)
 Route::post('/email/verification/resend', ResentEmailVerificationController::class)
     ->middleware(['auth:sanctum', 'throttle:6,1'])
     ->name('verification.send');
-
-Route::post('/forgot-password', ForgotPasswordController::class)
-    ->middleware('guest')
-    ->name('password.email');
-
-Route::post('/logout', LogoutController::class)
-    ->middleware('auth:sanctum')
-    ->name('logout');
-
-Route::post('/reset-password', ResetPasswordController::class)
-                ->middleware('guest')
-                ->name('password.store');
