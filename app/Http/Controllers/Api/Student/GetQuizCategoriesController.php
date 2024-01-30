4577be0ebd156be1b2b30ebaed51b5ab32e1ad5d
nishusapkota <nisa_csit2075@lict.edu.np>
2024-01-30 18:36:41 +0545

    Add filter to quiz categories in students api

--- app/Http/Controllers/Api/Student/GetQuizCategoriesController.php
+++ app/Http/Controllers/Api/Student/GetQuizCategoriesController.php
@@ -4,18 +4,28 @@ namespace App\Http\Controllers\Api\Student;
 
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use App\Http\Controllers\Controller;
+use App\Http\Repositories\QuizCategoryRepository;
+use App\Http\Requests\QuizCategoryFilterRequest;
 use App\Http\Resources\QuizCategoryResource;
 use App\Models\QuizCategory;
 
 class GetQuizCategoriesController extends Controller
 {
+    protected QuizCategoryRepository $quizCategoryRepository;
+
+    public function __construct(QuizCategoryRepository $quizCategoryRepository)
+    {
+        $this->quizCategoryRepository = $quizCategoryRepository;
+    }
     /**
      * @return AnonymousResourceCollection
      */
-    public function __invoke(): AnonymousResourceCollection
+    public function __invoke(QuizCategoryFilterRequest $request): AnonymousResourceCollection
     {
-        $quizCategories = QuizCategory::paginate(10);
-        return QuizCategoryResource::collection($quizCategories);
+        $data=$request->validated();
 
+        $quizCategories = $this->quizCategoryRepository->getFilteredQuizCategories($data);
+
+        return QuizCategoryResource::collection($quizCategories);
     }
 }
