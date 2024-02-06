c6e54f7e8dbc149cb770a18688be80c95ab3ccfc
kundan <kundan.karna1994@gmail.com>
2024-02-06 17:16:10 +0545

    fixed migration

--- database/migrations/2024_01_27_055315_add_avatar_to_profiles_table.php
+++ database/migrations/2024_01_27_055315_add_avatar_to_profiles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('profiles', function (Blueprint $table) {
-            $table->string('avatar')->after('career');
+            $table->string('avatar')->nullable()->after('career');
         });
     }
 
