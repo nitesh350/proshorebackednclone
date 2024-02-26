e139510e6ae3a94d3b47c652d95675cb3e01a73f
kundan <kundan.karna1994@gmail.com>
2024-02-26 14:31:11 +0545

    test import

--- tests/Feature/ImportQuestionTest.php
+++ tests/Feature/ImportQuestionTest.php
@@ -9,6 +9,7 @@ use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\File;
 
 class ImportQuestionTest extends TestCase
 {
@@ -19,22 +20,116 @@ class ImportQuestionTest extends TestCase
      *
      * @return void
      */
-    public function testImportQuestions()
+    public function test_admin_can_import_valid_questions()
     {
         $this->createAdminUser();
-        
-        $file = UploadedFile::fake()->create('questions.xlsx');
+        $header = ["Category", "Title", "Description", "option1", "option2", "option3", "option4", "option5", "option6", "Answer", "Weightage"];
+        $rows =  [
+            ["Test Category", "Test", "Test Description", "A", "B", "C", "D", "E", "F", "A", 5],
+        ];
+        $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $filePath = storage_path("app/imports/questions.xlsx");
 
-        Excel::fake();
-        $response = $this->actingAs($this->user)->post('api/admin/import-questions', [
-            'file' => $file,
-        ]);
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->fromArray([$header], NULL, 'A1');
+        foreach ($rows as $rowData) {
+            $sheet->fromArray($rowData, NULL, 'A' . ($sheet->getHighestRow() + 1));
+        }
+        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save($filePath);
 
-        Excel::assertImported('questions.xlsx');
 
-    
-     $response->assertStatus(200);
+        $file = new \Illuminate\Http\UploadedFile($filePath, 'questions.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', null, true);
+        $response = $this->actingAs($this->user)
+            ->post('api/admin/import-questions', ['file' => $file]);
 
-       $response->assertJson(['message' => 'Questions imported successfully']);
-     }
+        $response->assertStatus(200);
+
+        $response->assertJson(['message' => '1 imported Successfully. 0 duplicate record found']);
+        if (File::isDirectory(storage_path("app/imports"))) {
+            $files = Storage::files('imports');
+
+            foreach ($files as $file) {
+                Storage::delete($file);
+            }
+        }
+    }
+
+    /**
+     * Test import questions endpoint.
+     *
+     * @return void
+     */
+    public function test_admin_cannot_import_invalid_questions()
+    {
+        $this->createAdminUser();
+        $header = ["Category", "Title", "Description", "option1", "option2", "option3", "option4", "option5", "option6", "Answer", "Weightage"];
+        $rows =  [
+            ["Test Category", "Test", "Test Description", "A", "B", "C", "D", "E", "F", "A", 30],
+        ];
+        $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $filePath = storage_path("app/imports/questions.xlsx");
+
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->fromArray([$header], NULL, 'A1');
+        foreach ($rows as $rowData) {
+            $sheet->fromArray($rowData, NULL, 'A' . ($sheet->getHighestRow() + 1));
+        }
+        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save($filePath);
+
+
+        $file = new \Illuminate\Http\UploadedFile($filePath, 'questions.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', null, true);
+        $response = $this->actingAs($this->user)
+            ->post('api/admin/import-questions', ['file' => $file]);
+
+        $response->assertStatus(422);
+        if (File::isDirectory(storage_path("app/imports"))) {
+            $files = Storage::files('imports');
+
+            foreach ($files as $file) {
+                Storage::delete($file);
+            }
+        }
+    }
+
+    /**
+     * Test import questions endpoint.
+     *
+     * @return void
+     */
+    public function test_admin_cannot_import_duplicate_questions()
+    {
+        $this->createAdminUser();
+        $header = ["Category", "Title", "Description", "option1", "option2", "option3", "option4", "option5", "option6", "Answer", "Weightage"];
+        $rows =  [
+            ["Test Category", "Test", "Test Description", "A", "B", "C", "D", "E", "F", "A", 5],
+            ["Test Category", "Test", "Test Description", "A", "B", "C", "D", "E", "F", "A", 5],
+        ];
+        $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $filePath = storage_path("app/imports/questions.xlsx");
+
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->fromArray([$header], NULL, 'A1');
+        foreach ($rows as $rowData) {
+            $sheet->fromArray($rowData, NULL, 'A' . ($sheet->getHighestRow() + 1));
+        }
+        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save($filePath);
+
+
+        $file = new \Illuminate\Http\UploadedFile($filePath, 'questions.xlsx', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', null, true);
+        $response = $this->actingAs($this->user)
+            ->post('api/admin/import-questions', ['file' => $file]);
+
+        $response->assertStatus(200);
+        $response->assertJson(['message' => '1 imported Successfully. 1 duplicate record found']);
+        if (File::isDirectory(storage_path("app/imports"))) {
+            $files = Storage::files('imports');
+
+            foreach ($files as $file) {
+                Storage::delete($file);
+            }
+        }
+    }
 }
